efc0e071abfb7b28525c3250126f14b62b697d81
Derik <derik@oagro.com.br>
2020-11-11 17:55:09 -0300

    Creating round_icon_button and constants dart files.

---
+++ lib/components/round_icon_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class RoundIconButton extends StatelessWidget {
+  final IconData icon;
+  final Function onPressed;
+
+  RoundIconButton({@required this.icon, @required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: Icon(icon),
+      onPressed: onPressed,
+      constraints: BoxConstraints.tightFor(
+        height: 50.0,
+        width: 50.0,
+      ),
+      shape: CircleBorder(),
+      fillColor: Color(0xFF4C4F5E),
+    );
+  }
+}
---
+++ lib/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const Color kDarkColor = Color(0xff1d1e33);
+const Color kInactiveCardColor = Color(0xFF111328);
+const Color kPinkColor = Color(0xFFEB1555);
+const double kMinSliderValue = 120.0;
+const double kMaxSliderValue = 220.0;
+
+const TextStyle kLabelTextStyle = TextStyle(
+  color: Color(0xFF8D8E98),
+  fontSize: 18.0,
+);
+
+const TextStyle kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
